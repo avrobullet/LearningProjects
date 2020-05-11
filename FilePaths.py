a90f4e67599feb9e0c1e2fa2cfb08fe36642a8ad
avrobullet <douglasa@unbc.ca>
2020-05-11 11:26:22 -0700

    Updating with new projects

--- FilePaths.py
+++ FilePaths.py
@@ -1,4 +1,12 @@
 import os
+import csv
+import pandas as pd
 
-path = input()
-print(os.path.dirname(path))
+path = "D:/mutualaid.csv"
+df = pd.read_csv(path)
+#print(df)
+
+with open(path, 'r', newline='', encoding="utf8") as csvfile:
+    csv_reader = csv.reader(csvfile, quotechar='|')
+    print("CSV data row: %s", len(csv_reader))
+    #print("CSV data column: %s", % len(csv_reader))
--- test_bed.py
+++ test_bed.py
@@ -7,17 +7,15 @@ labels = np.array(
      [1, 1, 1],
      [1, 0, 1]]
 )
+
 N = np.size(labels,0)
+patient_1 = np.array(labels[0])
+# x = lambda a : a + 10
+def sensitivity(patient_1):
+    total_positives_tested = sum(testing_positive for testing_positive in patient_1 if testing_positive==1)
+    return np.true_divide(total_positives_tested, len(patient_1))
 
-test_array = [np.sum(labels, axis=0)]
-test_array_demo = np.array([(N-elem)/N for elem in np.array(np.sum(labels, axis=0))])
+specificity = lambda testing_negative:sum(testing_negative for testing_negative in patient_1
+                                                            if testing_negative==0)/len(patient_1)
 
-labels = list(np.sum(labels, axis=0))
-negative_class = np.array([(N-elem)/N for elem in labels])
-postive_class = np.array([elem/N for elem in test_array])
-print(test_array)
-print(labels)
-print(negative_class)
-print(postive_class)
-print("This:",test_array_demo)
-print(N)
+print(sensitivity(patient_1), specificity(patient_1))
